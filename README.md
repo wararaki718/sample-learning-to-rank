d2c729747c668b7ef5757b28817b010f85762dc6
wararaki <>
2019-12-21 03:17:15 +0900

    create images

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # sample-learning-to-rank
+
+## use package
+
+- https://github.com/o19s/elasticsearch-learning-to-rank
+- http://es-learn-to-rank.labs.o19s.com/
+
+## document
+
+- https://github.com/o19s/elasticsearch-learning-to-rank/tree/master/demo
---
+++ app/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd elasticsearch-learning-to-rank/demo
+
+## download data & ranklib
+python prepare.py
+
+## wait for statup the elasticsearch
+status='red'
+while [ "$status" != "green" ]
+do
+    echo "elasticsearch status: $status"
+    sleep 10
+    response=`curl ${ES_HOST}:${ES_PORT}/_cat/health?h=status`
+    status=`echo "$response" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g'`
+done
+
+python index_ml_tmdb.py
+
+echo "unused learning-to-rank" 
+python search.py Rambo
+
+python load_features.py
+
+python train.py
+
+echo "use learning-to-rank"
+python search.py Rambo
+
+echo "DONE"
